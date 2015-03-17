28ebf36cbc8763a907400599df5fd75f6c5b3d5f
Adam Patai <pataiadam@gmail.com>
2015-03-17 01:53:58 +0100

    Set global host and port

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 var http = require('http');
 var exec = require('child_process').exec;
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
@@ -22,7 +25,7 @@ http.createServer(function (req, res) {
             });
         }
     );
-}).listen(8080, '127.0.0.1');
+}).listen(server_port, server_ip_address);
 
 
 function htmlBuilder(content, callback){
